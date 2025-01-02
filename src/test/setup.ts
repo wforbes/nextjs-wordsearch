8fe011cc10992f60cbb0f2b99a1e900fa42febb2
Will Forbes <wforbes87@gmail.com>
2025-01-01 17:20:15 -0800

    add vitest setup file and mock next router with it

---
+++ src/test/setup.ts
@@ -0,0 +1,25 @@
+import { beforeAll, vi } from 'vitest';
+
+beforeAll(() => {
+  vi.mock('next/navigation', async (importOriginal) => {
+    const actual = await importOriginal<typeof import('next/navigation')>();
+    const { useRouter } = await vi.importActual<typeof import('next-router-mock')>('next-router-mock');
+    
+    const usePathname = vi.fn().mockImplementation(() => {
+      const router = useRouter();
+      return router.pathname;
+    });
+
+    const useSearchParams = vi.fn().mockImplementation(() => {
+      const router = useRouter();
+      return new URLSearchParams(router.query?.toString());
+    });
+
+    return {
+      ...actual,
+      useRouter: vi.fn().mockImplementation(useRouter),
+      usePathname,
+      useSearchParams,
+    };
+  });
+}); 
